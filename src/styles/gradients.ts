68257e9bd744ef2daf93b9af9ff30e5991476d79
marcofaggian <m@marcofaggian.com>
2020-06-27 12:53:10 +0200

    updated readme and improved css

--- src/styles/gradients.ts
+++ src/styles/gradients.ts
@@ -6,6 +6,7 @@ export const BgDiv = styled.div`
   position: absolute;
   height: 100%;
   width: 100%;
+  z-index: 2;
   background-image: ${({ colors }: { colors: Colors }) =>
     colors.map(
       ({ x, y, width, color }, i) =>
